42675bf4870157223d05392baa8f76ee66d7b09f
Isaac Melo <isaac.melo426@gmail.com>
2025-07-15 06:51:14 +0000

    fixed file tree

---
+++ utils/move2.sh
@@ -0,0 +1,19 @@
+cd ../xschem
+
+mkdir caparray
+mv capacitor_array.sch caparray/
+mv capacitor_array.sym caparray/
+
+mv strongarmLatch.sch vault/
+mv strongarmLatch.sym vault/
+mv strongarmtb.sch vault/
+
+mv sar.sch vault/
+
+mv Buffer.sch vault/
+
+mv bstest.sch vault/
+
+mv comp_test.sch vault/
+
+mv switch_test.sch vault/
